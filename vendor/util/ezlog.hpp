5bdcd09e01afbab800bfa1641778306ba9bb481d
Eva Sanders <maeve.modus@gmail.com>
2025-06-24 22:16:33 -0600

    Moved ezlog to header only

--- vendor/util/ezlog.hpp
+++ vendor/util/ezlog.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <iostream>
+#include <csignal>
 #include <string>
 #include <format>
 #include <map>
@@ -26,21 +28,48 @@ namespace ezlog
     {
     public:
         // API/public methods
-        static void LogERROR(const std::string& message);
-        static void LogWARN(const std::string& message);
-        static void LogINFO(const std::string& message);
+        static void LogERROR(const std::string& message)
+        {
+            LogMessage(LogLevel::ERROR, "ERROR", message);
+            // Signal stop
+            std::raise(SIGINT);
+        }
+        static void LogWARN(const std::string& message)
+        {
+            LogMessage(LogLevel::WARN, "WARN", message);
+        }
+        static void LogINFO(const std::string& message)
+        {
+            LogMessage(LogLevel::INFO, "INFO", message);
+        }
 
         inline static void ASSERT(bool x, const std::string& message) { if(!x) { LogERROR(message); } }
 
-        static void SetColor(LogLevel level, LoggerColor color);
+        static void SetColor(LogLevel level, LoggerColor color)
+        {
+            s_Colors[level] = color;
+        }
 
     private: // methods
-        static void LogMessage(LogLevel level, const std::string& tag, const std::string& message);
+        static void LogMessage(LogLevel level, const std::string& tag, const std::string& message)
+        {
+            if (level <= s_LogLevel)
+                std::cout << "[" << s_LogColors[s_Colors[level]] << tag << s_LogColors[LoggerColor::NORMAL] << "]::" << message << std::endl;
+        }
 
     private: // data
-        static LogLevel s_LogLevel;
-        static std::map<LogLevel, LoggerColor> s_Colors;
-        static std::map<LoggerColor, const char*> s_LogColors;
-
+        inline static LogLevel s_LogLevel = LogLevel::INFO;
+        inline static std::map<LogLevel, LoggerColor> s_Colors = {
+            { LogLevel::NONE,  LoggerColor::NORMAL },
+            { LogLevel::ERROR, LoggerColor::NORMAL },
+            { LogLevel::WARN,  LoggerColor::NORMAL },
+            { LogLevel::INFO,  LoggerColor::NORMAL },
+        };
+        inline static std::map<LoggerColor, const char*> s_LogColors = {
+            { LoggerColor::NORMAL, "\e[0m" },
+            { LoggerColor::RED,    "\e[31m" },
+            { LoggerColor::YELLOW, "\e[33m" },
+            { LoggerColor::GREEN,  "\e[32m" },
+        };
     };
 }
